88d1032e7584e1cf1062a2aea68a823491395f26
qweasdzxc1997 <thanhtung02697@gmail.com>
2022-12-19 17:28:48 +0700

    source

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Baitapthuchanh from "./baitapthuchanh"
+import Header from './baitapthuchanh/header';
+import Footer from './baitapthuchanh/footer';
+import Body from './baitapthuchanh/body';
+import Banner from './baitapthuchanh/banner';
+import Item from './baitapthuchanh/item';
+
+
+
+function App() {
+  return (
+    <div >
+      <Baitapthuchanh/> 
+    </div>
+  );
+}
+
+export default App;
